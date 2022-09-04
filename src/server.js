ec490ff85c69beea05c49c5669109d76f3163c6d
Taishi1020 <tai.yakiman1020@gmail.com>
2022-09-04 20:56:51 +0900

    スキーマの定義の拡張

--- src/server.js
+++ src/server.js
@@ -3,16 +3,32 @@
 
 const {ApolloServer, gql} = require("apollo-server");
 
+let links = [
+    {
+        id: "link-0",
+        description: "Graphqlを学んでいます。",
+        url: "www.graphql-tutorial.com",
+    }
+]
+
 //GraphQLのスキーマ定義
 const typeDefs = gql`
  type Query {
    info: String!
+   feed: [Link]!
+ }
+ type Link {
+ id: ID!
+ description: string!
+ url: String!
  }
 `;
+
 
 const resolvers = {
     Query: {
         info: () => "HackerNewsクローン",
+        feed: () => links,
     },
 }
 
@@ -20,5 +36,6 @@ const server = new ApolloServer({
     typeDefs,
     resolvers,
 })
+
 
 server.listen().then(({url}) => console.log(`${url}でサーバーを起動中・・・`));
