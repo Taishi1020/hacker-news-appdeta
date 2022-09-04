9eee12e63cfd2f4076ea1dcee52720a9f71a2df9
Taishi1020 <tai.yakiman1020@gmail.com>
2022-09-04 19:00:23 +0900

    Graphql、Apollo設定

---
+++ src/server.js
@@ -0,0 +1,26 @@
+//ドキュメント参照
+//https://www.apollographql.com/docs/apollo-server/getting-started
+
+const {ApolloServer, gql} = require("apollo-server");
+
+//GraphQLのスキーマ定義
+const typeDefs = gql`
+ type Query {
+   info: String!
+ }
+`;
+
+const resolvers = {
+    Query: {
+        info: () => "HackerNewsクローン",
+    },
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+server
+    .listen()
+    .then(({url}) => console.log(`${url}でサーバーを起動中・・・`));
